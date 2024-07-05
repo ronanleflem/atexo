4e37d7e7a84b9d58db866df98ec9e86fac0c99ca
ronanleflem <ronan.leflem@gmail.com>
2024-07-05 16:19:58 +0200

    Skeleton Atexo Backend

---
+++ src/main/java/com/backend/atexo/AtexoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.atexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtexoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/atexo/AtexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.atexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
